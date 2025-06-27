404719a5c2edd91db18d1580c6d40749eeb4649d
peiwei.yao <peiwei.yao@coolkit.cn>
2025-06-27 15:56:36 +0800

    init repo

---
+++ build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e
+
+function lb_config_init () {
+    sudo lb config --distribution bookworm \
+        --archive-areas "main contrib" \
+        --mirror-bootstrap "http://mirrors.ustc.edu.cn/debian" \
+        --mirror-binary "http://mirrors.ustc.edu.cn/debian" \
+        --mirror-binary-security "http://mirrors.ustc.edu.cn/debian" \
+        --mirror-chroot "http://mirrors.ustc.edu.cn/debian" \
+        --mirror-chroot-security "http://mirrors.ustc.edu.cn/debian" \
+        --initramfs none \
+        --binary-filesystem ext4 \
+        --binary-image iso \
+        --chroot-filesystem squashfs \
+        --security false \
+        --mode debian \
+        --architectures amd64 \
+        --debug \
+        --apt aptitude  
+}
+
+
+function lb_build () {
+    sudo lb build
+}
+
+case $1 in
+    config)
+        lb_config_init
+        ;;
+    build)
+        lb_build
+        ;;
+    *)
+        echo "Usage: $0 {config|build}"
+        ;;
+esac    
